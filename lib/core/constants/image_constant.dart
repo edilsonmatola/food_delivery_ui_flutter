fc832cb5419158be09d5a33fa411f84d97ea8ba1
Edilson Matola <edilsonmatola13@gmail.com>
2022-12-21 07:59:48 +0200

    Image Path file

---
+++ lib/core/constants/image_constant.dart
@@ -0,0 +1,18 @@
+class ImageConstant {
+  // * Food Images
+  static const String burritoImage = 'assets/images/burrito.jpg';
+  static const String steakImage = 'assets/images/steak.jpg';
+  static const String pastaImage = 'assets/images/pasta.jpg';
+  static const String ramenImage = 'assets/images/ramen.jpg';
+  static const String pancakesImage = 'assets/images/pancakes.jpg';
+  static const String burgerImage = 'assets/images/burger.jpg';
+  static const String pizzaImage = 'assets/images/pizza.jpg';
+  static const String salmonImage = 'assets/images/salmon.jpg';
+
+  // * Restaurant Menus
+  static const String restaurant0 = 'assets/images/restaurant0.jpg';
+  static const String restaurant1 = 'assets/images/restaurant1.jpg';
+  static const String restaurant2 = 'assets/images/restaurant2.jpg';
+  static const String restaurant3 = 'assets/images/restaurant3.jpg';
+  static const String restaurant4 = 'assets/images/restaurant4.jpg';
+}
