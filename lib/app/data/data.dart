67c56a5b2ec94d75b6f1d2fe1efd5c01f4db336d
Edilson Matola <edilsonmatola13@gmail.com>
2022-12-21 07:40:17 +0200

    Code refactor

--- lib/app/data/data.dart
+++ lib/app/data/data.dart
@@ -1,56 +1,56 @@
-import '../../models/food.dart';
-import '../../models/order.dart';
-import '../../models/restaurant.dart';
-import 'models/user.dart';
+import '../../models/food_model.dart';
+import '../../models/order_model.dart';
+import '../../models/restaurant_model.dart';
+import 'models/user_model.dart';
 
 // Food
-final _burrito =
-    Food(imageUrl: 'assets/images/burrito.jpg', name: 'Burrito', price: 8.99);
+final _burrito = FoodModel(
+    imageUrl: 'assets/images/burrito.jpg', name: 'Burrito', price: 8.99);
 final _steak =
-    Food(imageUrl: 'assets/images/steak.jpg', name: 'Steak', price: 17.99);
+    FoodModel(imageUrl: 'assets/images/steak.jpg', name: 'Steak', price: 17.99);
 final _pasta =
-    Food(imageUrl: 'assets/images/pasta.jpg', name: 'Pasta', price: 14.99);
+    FoodModel(imageUrl: 'assets/images/pasta.jpg', name: 'Pasta', price: 14.99);
 final _ramen =
-    Food(imageUrl: 'assets/images/ramen.jpg', name: 'Ramen', price: 13.99);
-final _pancakes =
-    Food(imageUrl: 'assets/images/pancakes.jpg', name: 'Pancakes', price: 9.99);
-final _burger =
-    Food(imageUrl: 'assets/images/burger.jpg', name: 'Burger', price: 14.99);
+    FoodModel(imageUrl: 'assets/images/ramen.jpg', name: 'Ramen', price: 13.99);
+final _pancakes = FoodModel(
+    imageUrl: 'assets/images/pancakes.jpg', name: 'Pancakes', price: 9.99);
+final _burger = FoodModel(
+    imageUrl: 'assets/images/burger.jpg', name: 'Burger', price: 14.99);
 final _pizza =
-    Food(imageUrl: 'assets/images/pizza.jpg', name: 'Pizza', price: 11.99);
-final _salmon = Food(
+    FoodModel(imageUrl: 'assets/images/pizza.jpg', name: 'Pizza', price: 11.99);
+final _salmon = FoodModel(
     imageUrl: 'assets/images/salmon.jpg', name: 'Salmon Salad', price: 12.99);
 
 // Restaurants
-final _restaurant0 = Restaurant(
+final _restaurant0 = RestaurantModel(
   imageUrl: 'assets/images/restaurant0.jpg',
   name: 'Restaurant 0',
   address: '200 Main St, New York, NY',
   rating: 5,
   menu: [_burrito, _steak, _pasta, _ramen, _pancakes, _burger, _pizza, _salmon],
 );
-final _restaurant1 = Restaurant(
+final _restaurant1 = RestaurantModel(
   imageUrl: 'assets/images/restaurant1.jpg',
   name: 'Restaurant 1',
   address: '200 Main St, New York, NY',
   rating: 4,
   menu: [_steak, _pasta, _ramen, _pancakes, _burger, _pizza],
 );
-final _restaurant2 = Restaurant(
+final _restaurant2 = RestaurantModel(
   imageUrl: 'assets/images/restaurant2.jpg',
   name: 'Restaurant 2',
   address: '200 Main St, New York, NY',
   rating: 4,
   menu: [_steak, _pasta, _pancakes, _burger, _pizza, _salmon],
 );
-final _restaurant3 = Restaurant(
+final _restaurant3 = RestaurantModel(
   imageUrl: 'assets/images/restaurant3.jpg',
   name: 'Restaurant 3',
   address: '200 Main St, New York, NY',
   rating: 2,
   menu: [_burrito, _steak, _burger, _pizza, _salmon],
 );
-final _restaurant4 = Restaurant(
+final _restaurant4 = RestaurantModel(
   imageUrl: 'assets/images/restaurant4.jpg',
   name: 'Restaurant 4',
   address: '200 Main St, New York, NY',
@@ -58,7 +58,7 @@ final _restaurant4 = Restaurant(
   menu: [_burrito, _ramen, _pancakes, _salmon],
 );
 
-final List<Restaurant> restaurants = [
+final List<RestaurantModel> restaurants = [
   _restaurant0,
   _restaurant1,
   _restaurant2,
@@ -67,67 +67,67 @@ final List<Restaurant> restaurants = [
 ];
 
 // User
-final currentUser = User(
+final currentUser = UserModel(
   name: 'Rebecca',
   orders: [
-    Order(
-      date: 'Nov 10, 2019',
+    OrderModel(
+      dateTime: 'Nov 10, 2019',
       food: _steak,
       restaurant: _restaurant2,
       quantity: 1,
     ),
-    Order(
-      date: 'Nov 8, 2019',
+    OrderModel(
+      dateTime: 'Nov 8, 2019',
       food: _ramen,
       restaurant: _restaurant0,
       quantity: 3,
     ),
-    Order(
-      date: 'Nov 5, 2019',
+    OrderModel(
+      dateTime: 'Nov 5, 2019',
       food: _burrito,
       restaurant: _restaurant1,
       quantity: 2,
     ),
-    Order(
-      date: 'Nov 2, 2019',
+    OrderModel(
+      dateTime: 'Nov 2, 2019',
       food: _salmon,
       restaurant: _restaurant3,
       quantity: 1,
     ),
-    Order(
-      date: 'Nov 1, 2019',
+    OrderModel(
+      dateTime: 'Nov 1, 2019',
       food: _pancakes,
       restaurant: _restaurant4,
       quantity: 1,
     ),
   ],
   cart: [
-    Order(
-      date: 'Nov 11, 2019',
+    OrderModel(
+      dateTime: 'Nov 11, 2019',
       food: _burger,
       restaurant: _restaurant2,
       quantity: 2,
     ),
-    Order(
-      date: 'Nov 11, 2019',
+    OrderModel(
+      dateTime: 'Nov 11, 2019',
       food: _pasta,
       restaurant: _restaurant2,
       quantity: 1,
     ),
-    Order(
-      date: 'Nov 11, 2019',
+    OrderModel(
+      dateTime: 'Nov 11, 2019',
       food: _salmon,
       restaurant: _restaurant3,
       quantity: 1,
     ),
-    Order(
-      date: 'Nov 11, 2019',
+    OrderModel(
+      dateTime: 'Nov 11, 2019',
       food: _pancakes,
       restaurant: _restaurant4,
       quantity: 3,
     ),
-    Order(
-      date: 'Nov 11, 2019',
+    OrderModel(
+      dateTime: 'Nov 11, 2019',
       food: _burrito,
       restaurant: _restaurant1,
       quantity: 2,
