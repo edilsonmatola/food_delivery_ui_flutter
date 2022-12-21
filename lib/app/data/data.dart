c02c9ca3f7324fb9db379fd053557a642265cc42
Edilson Matola <edilsonmatola13@gmail.com>
2022-12-21 08:02:42 +0200

    Applied ImageConstant class

--- lib/app/data/data.dart
+++ lib/app/data/data.dart
@@ -1,3 +1,4 @@
+import '../../core/core_export.dart';
 import '../../models/food_model.dart';
 import '../../models/order_model.dart';
 import '../../models/restaurant_model.dart';
@@ -5,53 +6,53 @@ import 'models/user_model.dart';
 
 // Food
 final _burrito = FoodModel(
-    imageUrl: 'assets/images/burrito.jpg', name: 'Burrito', price: 8.99);
+    imageUrl: ImageConstant.burritoImage, name: 'Burrito', price: 8.99);
 final _steak =
-    FoodModel(imageUrl: 'assets/images/steak.jpg', name: 'Steak', price: 17.99);
+    FoodModel(imageUrl: ImageConstant.steakImage, name: 'Steak', price: 17.99);
 final _pasta =
-    FoodModel(imageUrl: 'assets/images/pasta.jpg', name: 'Pasta', price: 14.99);
+    FoodModel(imageUrl: ImageConstant.pastaImage, name: 'Pasta', price: 14.99);
 final _ramen =
-    FoodModel(imageUrl: 'assets/images/ramen.jpg', name: 'Ramen', price: 13.99);
+    FoodModel(imageUrl: ImageConstant.ramenImage, name: 'Ramen', price: 13.99);
 final _pancakes = FoodModel(
-    imageUrl: 'assets/images/pancakes.jpg', name: 'Pancakes', price: 9.99);
+    imageUrl: ImageConstant.pancakesImage, name: 'Pancakes', price: 9.99);
 final _burger = FoodModel(
-    imageUrl: 'assets/images/burger.jpg', name: 'Burger', price: 14.99);
+    imageUrl: ImageConstant.burgerImage, name: 'Burger', price: 14.99);
 final _pizza =
-    FoodModel(imageUrl: 'assets/images/pizza.jpg', name: 'Pizza', price: 11.99);
+    FoodModel(imageUrl: ImageConstant.pizzaImage, name: 'Pizza', price: 11.99);
 final _salmon = FoodModel(
-    imageUrl: 'assets/images/salmon.jpg', name: 'Salmon Salad', price: 12.99);
+    imageUrl: ImageConstant.salmonImage, name: 'Salmon Salad', price: 12.99);
 
 // Restaurants
 final _restaurant0 = RestaurantModel(
-  imageUrl: 'assets/images/restaurant0.jpg',
+  imageUrl: ImageConstant.restaurant0,
   name: 'Restaurant 0',
   address: '200 Main St, New York, NY',
   rating: 5,
   menu: [_burrito, _steak, _pasta, _ramen, _pancakes, _burger, _pizza, _salmon],
 );
 final _restaurant1 = RestaurantModel(
-  imageUrl: 'assets/images/restaurant1.jpg',
+  imageUrl: ImageConstant.restaurant1,
   name: 'Restaurant 1',
   address: '200 Main St, New York, NY',
   rating: 4,
   menu: [_steak, _pasta, _ramen, _pancakes, _burger, _pizza],
 );
 final _restaurant2 = RestaurantModel(
-  imageUrl: 'assets/images/restaurant2.jpg',
+  imageUrl: ImageConstant.restaurant2,
   name: 'Restaurant 2',
   address: '200 Main St, New York, NY',
   rating: 4,
   menu: [_steak, _pasta, _pancakes, _burger, _pizza, _salmon],
 );
 final _restaurant3 = RestaurantModel(
-  imageUrl: 'assets/images/restaurant3.jpg',
+  imageUrl: ImageConstant.restaurant3,
   name: 'Restaurant 3',
   address: '200 Main St, New York, NY',
   rating: 2,
   menu: [_burrito, _steak, _burger, _pizza, _salmon],
 );
 final _restaurant4 = RestaurantModel(
-  imageUrl: 'assets/images/restaurant4.jpg',
+  imageUrl: ImageConstant.restaurant4,
   name: 'Restaurant 4',
   address: '200 Main St, New York, NY',
   rating: 3,
