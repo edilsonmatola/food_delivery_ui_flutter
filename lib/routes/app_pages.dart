5ef423ef4a6e20b9a155c938ba5aff8110b72b7a
Edilson Matola <edilsonmatola13@gmail.com>
2023-02-11 23:38:26 +0200

    Updated App Pages

---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,23 @@
+import 'package:get/get.dart';
+
+import '../app/presentation/cart_screen/cart_screen.dart';
+import '../app/presentation/home_screen/home_screen.dart';
+import '../app/presentation/restaurant_screen/restaurant_screen.dart';
+import 'app_routes.dart';
+
+class AppPages {
+  static final appPages = [
+    GetPage(
+      name: AppRoutes.cartRoute,
+      page: () => CartScreen(),
+    ),
+    GetPage(
+      name: AppRoutes.homeRoute,
+      page: () => HomeScreen(),
+    ),
+    GetPage(
+      name: AppRoutes.restaurantRoute,
+      page: () => RestaurantScreen(),
+    ),
+  ];
+}
