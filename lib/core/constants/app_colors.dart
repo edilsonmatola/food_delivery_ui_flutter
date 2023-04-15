c22a87e31fe0e0f1bebc9a0d74ccbc8667c3dc01
Edilson Matola <edilsonmatola13@gmail.com>
2023-04-15 19:14:19 +0200

    Big features (#11)
    * Removed old lint rules
    
    * Updated Lint Rules
    
    * Const App colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final primaryColor = fromHex('#22A45');
+  static final secondaryColor = fromHex('#22A45');
+  static final neutralBlack = fromHex('#010F07');
+
+  static Color fromHex(String hexString) {
+    final buffer = StringBuffer();
+    if (hexString.length == 6 || hexString.length == 7) {
+      buffer.write('ff');
+    }
+    buffer.write(hexString.replaceFirst('#', ''));
+    return Color(
+      int.parse(buffer.toString(), radix: 16),
+    );
+  }
+}
