f59bd316f3e29f23a544cb267b758f65b13ed405
Edilson Matola <edilsonmatola13@gmail.com>
2023-04-15 17:59:38 +0200

    Renamed files

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -2,23 +2,23 @@
 
 import 'package:get/get.dart';
 
-import '../app/presentation/cart_screen/cart_screen.dart';
-import '../app/presentation/home_screen/home_screen.dart';
-import '../app/presentation/restaurant_screen/restaurant_screen.dart';
+import '../app/features/cart/cart_page.dart';
+import '../app/features/home/home_page.dart';
+import '../app/features/restaurant/restaurant_page.dart';
 
 class AppPages {
   static final appPages = [
     GetPage(
       name: AppRoutes.cartRoute,
-      page: () => CartScreen(),
+      page: () => CartPage(),
     ),
     GetPage(
       name: AppRoutes.homeRoute,
-      page: () => HomeScreen(),
+      page: () => HomePage(),
     ),
     GetPage(
       name: AppRoutes.restaurantRoute,
-      page: () => RestaurantScreen(),
+      page: () => RestaurantPage(),
     ),
   ];
 }
