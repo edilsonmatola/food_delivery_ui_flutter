b76da41990e76c2edf443f21cee8744ee7ad44a1
Edilson Matola <edilsonmatola13@gmail.com>
2023-04-15 17:56:12 +0200

    Moved files

---
+++ lib/app/features/home/widgets/recent_orders.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+
+import '../../../data/data.dart';
+import '../../../data/data_export.dart';
+
+class RecentOrders extends StatelessWidget {
+  const RecentOrders({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Text(
+            'Recent Orders',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 1.2,
+            ),
+          ),
+        ),
+        Container(
+          height: 120,
+          child: ListView.builder(
+            physics: BouncingScrollPhysics(),
+            padding: EdgeInsets.only(left: 10),
+            scrollDirection: Axis.horizontal,
+            itemCount: currentUser.orders!.length,
+            itemBuilder: (BuildContext context, int index) {
+              final order = currentUser.orders![index];
+              return _RecentOrderCard(order: order);
+            },
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _RecentOrderCard extends StatelessWidget {
+  const _RecentOrderCard({
+    Key? key,
+    required this.order,
+  }) : super(key: key);
+
+  final OrderModel order;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      width: 320,
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 1,
+            spreadRadius: 4,
+            color: Colors.grey.shade100,
+            // offset: Offset(0, 2),
+          ),
+        ],
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(
+          color: Colors.grey[200]!,
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image(
+                    height: 100,
+                    width: 100,
+                    image: AssetImage(
+                      order.food!.imageUrl!,
+                    ),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(12),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          order.food!.name!,
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          order.restaurant!.name!,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          order.dateTime!,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w600,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 20),
+            width: 48,
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(30),
+            ),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.add,
+              ),
+              iconSize: 30,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
