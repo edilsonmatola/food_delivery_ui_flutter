9b85ba324c47c491edd4e9857e915d7c4e40cc1f
Edilson Matola <edilsonmatola13@gmail.com>
2023-04-27 15:57:09 +0200

    Code refactor

--- lib/app/ui/features/walkthrough/walkthrough_page3.dart
+++ lib/app/ui/features/walkthrough/walkthrough_page3.dart
@@ -14,17 +14,45 @@ class WalkthroughPage3 extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         body: SingleChildScrollView(
+          padding: const EdgeInsets.only(
+            top: Sizes.p32,
+          ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               SvgPicture.asset(
                 AppAssets.walktrhoughImage3,
                 width: 375,
                 height: 300,
               ),
-              gapH64,
-              PrimaryButton(
-                title: 'NEXT',
-                onPressed: () => Get.toNamed(AppRoutes.homeRoute),
+              gapH32,
+              Text(
+                Strings.walkthrough3Title,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  color: AppColors.neutralBlack,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              gapH20,
+              Text(
+                Strings.walkthrough3Description,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: AppColors.neutralGrey,
+                  fontSize: 16,
+                ),
+              ),
+              gapH32,
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Sizes.p20,
+                ),
+                child: PrimaryButton(
+                  title: 'NEXT',
+                  onPressed: () => Get.toNamed(AppRoutes.homeRoute),
+                ),
               ),
             ],
           ),
