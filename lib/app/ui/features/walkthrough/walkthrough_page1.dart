e324970e7abf6138ad8413286c31a9cbe5f20ae9
Edilson Matola <edilsonmatola13@gmail.com>
2023-04-27 15:46:57 +0200

    Code refactor

---
+++ lib/app/ui/features/walkthrough/walkthrough_page1.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+
+import '../../../core/core_export.dart';
+import '../../../routes/app_pages.dart';
+import '../../common_widgets/buttons/primary_button.dart';
+
+class WalkthroughPage1 extends StatelessWidget {
+  const WalkthroughPage1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.only(
+            top: Sizes.p32,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SvgPicture.asset(
+                AppAssets.walktrhoughImage1,
+                width: 375,
+                height: 300,
+              ),
+              gapH32,
+              Text(
+                Strings.walkthrough1Title,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  color: AppColors.neutralBlack,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              gapH20,
+              Text(
+                Strings.walkthrough1Description,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: AppColors.neutralGrey,
+                  fontSize: 16,
+                ),
+              ),
+              gapH32,
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: Sizes.p20,
+                ),
+                child: PrimaryButton(
+                  title: 'NEXT',
+                  onPressed: () => Get.toNamed(AppRoutes.walkthrough2Route),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
