7a1513d3b79ec01cceb960ec699a6aee5c7d2483
Edilson Matola <edilsonmatola13@gmail.com>
2023-04-27 15:47:14 +0200

    Constant classes

---
+++ lib/app/core/constants/app_strings.dart
@@ -0,0 +1,11 @@
+class Strings {
+  static const walkthrough1Title = 'All your favorites';
+  static const walkthrough1Description =
+      'Order from the best local restaurants with easy, on-demand delivery.';
+  static const walkthrough2Title = 'Free delivery offers';
+  static const walkthrough2Description =
+      'Free delivery for new customers via Apple Pay and others payment methods.';
+  static const walkthrough4Title = 'Choose your food';
+  static const walkthrough3Description =
+      "Easily find your type of food craving and you'll get delivery in wide range.";
+}
---
+++ lib/app/core/constants/constants_export.dart
@@ -0,0 +1,4 @@
+export 'app_assets.dart';
+export 'app_colors.dart';
+export 'app_sizes.dart';
+export 'app_strings.dart';
