38e9deac634645ef51f0323fe566ca9c57b08153
Edilson Matola <edilsonmatola13@gmail.com>
2023-06-29 12:42:17 +0200

    Code refactor

--- lib/app/ui/common_widgets/buttons/primary_button.dart
+++ lib/app/ui/common_widgets/buttons/primary_button.dart
@@ -18,25 +18,21 @@ class PrimaryButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: width ?? 150,
-      height: height ?? 45,
-      child: ElevatedButton(
-        onPressed: onPressed,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Theme.of(context).primaryColor,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(
-              Sizes.p12,
-            ),
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Theme.of(context).primaryColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            Sizes.p12,
           ),
         ),
-        child: Text(
-          title,
-          style: const TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-          ),
+      ),
+      child: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 20,
         ),
       ),
     );
