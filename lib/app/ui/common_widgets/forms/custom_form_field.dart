d65059341187c6d5b86fdbefe8fce7d92da83aba
Edilson Matola <edilsonmatola13@gmail.com>
2023-06-29 12:42:30 +0200

    Add attributes

--- lib/app/ui/common_widgets/forms/custom_form_field.dart
+++ lib/app/ui/common_widgets/forms/custom_form_field.dart
@@ -6,10 +6,16 @@ class CustomFormField extends StatefulWidget {
     super.key,
     this.keyboardType,
     this.hintText,
+    this.label,
+    this.formFieldKey,
+    this.controller,
   });
 
+  final GlobalKey<FormFieldState>? formFieldKey;
+  final TextEditingController? controller;
   final TextInputType? keyboardType;
   final String? hintText;
+  final String? label;
 
   @override
   State<CustomFormField> createState() => _CustomFormFieldState();
@@ -19,12 +25,19 @@ class _CustomFormFieldState extends State<CustomFormField> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      key: widget.formFieldKey,
+      controller: widget.controller,
       style: const TextStyle(
         color: Colors.black,
         fontSize: 16,
       ),
       keyboardType: widget.keyboardType,
       decoration: InputDecoration(
+        labelText: widget.label,
+        floatingLabelStyle: const TextStyle(
+          color: Colors.black,
+          fontSize: 22,
+        ),
         hintText: widget.hintText,
         hintStyle: TextStyle(
           color: AppColors.neutralGrey,
@@ -43,6 +56,12 @@ class _CustomFormFieldState extends State<CustomFormField> {
           ),
         ),
         focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(6),
+          borderSide: const BorderSide(
+            color: Colors.transparent,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(6),
           borderSide: const BorderSide(
             color: Colors.transparent,
